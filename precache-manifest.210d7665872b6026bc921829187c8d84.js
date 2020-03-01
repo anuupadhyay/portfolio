5b019a9acc1e9e9054a96b5070d1479c6b2c1ef6
Anu Upadhyay <anu.upadhyay.1994@gmail.com>
2020-03-02 00:19:41 +0530

    Updates

---
+++ precache-manifest.210d7665872b6026bc921829187c8d84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0cdb4a4286bb36db6f4302cc58e6b29",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "d1f4835797cbebf0e66d",
+    "url": "/portfolio/static/css/main.83bd8ac9.chunk.css"
+  },
+  {
+    "revision": "724f70080248f184b896",
+    "url": "/portfolio/static/js/2.f5bb07c4.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/portfolio/static/js/2.f5bb07c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1f4835797cbebf0e66d",
+    "url": "/portfolio/static/js/main.63c7069c.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  }
+]);
