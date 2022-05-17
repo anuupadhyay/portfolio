820bd03992ed7114fc4df27e8bfa2768bd9885ad
Anu <anu.upadhyay.1994@gmail.com>
2022-05-17 23:09:05 +0530

    Updates

---
+++ precache-manifest.b2b3de8773d79419bc5ea9727e6340f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4198edeb3cc139972d89bc8c1eccb89",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "46de42660b5434c8ca1e",
+    "url": "/portfolio/static/css/main.83bd8ac9.chunk.css"
+  },
+  {
+    "revision": "724f70080248f184b896",
+    "url": "/portfolio/static/js/2.f5bb07c4.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/portfolio/static/js/2.f5bb07c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46de42660b5434c8ca1e",
+    "url": "/portfolio/static/js/main.fe21891c.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  }
+]);
